329ed83b26c690e4d84417caffac08463aff0d02
Ananda Kumar <anandakumarpagadala04@gmail.com>
2026-03-31 19:11:22 -0500

    Add Terraform VPC networking resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,3 +6,32 @@ resource "aws_ecr_repository" "transaction_service_tf" {
     scan_on_push = true
   }
 }
+
+resource "aws_vpc" "devops_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "devops-project-vpc"
+  }
+}
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.devops_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-1"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.devops_vpc.id
+
+  tags = {
+    Name = "devops-igw"
+  }
+}
